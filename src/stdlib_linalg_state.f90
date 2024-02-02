a4f77572fce35cb2449ef07ca882dc32b9a64b92
Federico Perini <federico.perini@gmail.com>
2024-02-02 08:55:28 +0100

    add state comparison operators

--- src/stdlib_linalg_state.f90
+++ src/stdlib_linalg_state.f90
@@ -3,6 +3,11 @@ module stdlib_linalg_state
      implicit none(type, external)
      public
 
+     !> Public interfaces
+     public :: operator(==),operator(/=)
+     public :: operator(<),operator(<=)
+     public :: operator(>),operator(>=)
+
      !> State return types
      integer(ilp), parameter :: LINALG_SUCCESS = 0_ilp
 
@@ -11,7 +16,8 @@ module stdlib_linalg_state
      integer(ilp), parameter, private :: NAME_LENGTH = 32_ilp
 
      !> `stdlib_state` defines a state return type for a
-     !> linear algebra routine
+     !> linear algebra routine. State contains a status flag, a comment, and a
+     !> procedure specifier that can be used to mark where the error happened
      type, public :: stdlib_state
 
          !> The current exit state
@@ -32,6 +38,32 @@ module stdlib_linalg_state
 
      end type stdlib_state
 
+     !> Comparison operators
+     interface operator(==)
+         module procedure state_eq_flag
+         module procedure flag_eq_state
+     end interface
+     interface operator(/=)
+         module procedure state_neq_flag
+         module procedure flag_neq_state
+     end interface
+     interface operator(<)
+         module procedure state_lt_flag
+         module procedure flag_lt_state
+     end interface
+     interface operator(<=)
+         module procedure state_le_flag
+         module procedure flag_le_state
+     end interface
+     interface operator(>)
+         module procedure state_gt_flag
+         module procedure flag_gt_state
+     end interface
+     interface operator(>=)
+         module procedure state_ge_flag
+         module procedure flag_ge_state
+     end interface
+
 
      contains
 
@@ -46,5 +78,67 @@ module stdlib_linalg_state
         class(stdlib_state), intent(in) :: this
         state_is_error = this%state/=LINALG_SUCCESS
      end function state_is_error
+
+     !> Compare an error flag with an integer
+     elemental logical function state_eq_flag(err,flag)
+        type(stdlib_state), intent(in) :: err
+        integer, intent(in) :: flag
+        state_eq_flag = err%ierr == flag
+     end function state_eq_flag
+     elemental logical function flag_eq_state(flag,err)
+        integer, intent(in) :: flag
+        type(stdlib_state), intent(in) :: err
+        flag_eq_state = err%ierr == flag
+     end function flag_eq_state
+     elemental logical function state_neq_flag(err,flag)
+        type(stdlib_state), intent(in) :: err
+        integer, intent(in) :: flag
+        state_neq_flag = .not.state_eq_flag(err,flag)
+     end function state_neq_flag
+     elemental logical function flag_neq_state(flag,err)
+        integer, intent(in) :: flag
+        type(stdlib_state), intent(in) :: err
+        state_neq_flag = .not.state_eq_flag(err,flag)
+     end function flag_neq_state
+     elemental logical function state_lt_flag(err,flag)
+        type(stdlib_state), intent(in) :: err
+        integer, intent(in) :: flag
+        state_lt_flag = err%ierr < flag
+     end function state_lt_flag
+     elemental logical function state_le_flag(err,flag)
+        type(stdlib_state), intent(in) :: err
+        integer, intent(in) :: flag
+        state_le_flag = err%ierr <= flag
+     end function state_le_flag
+     elemental logical function flag_lt_state(flag,err)
+        integer, intent(in) :: flag
+        type(stdlib_state), intent(in) :: err
+        flag_lt_state = err%ierr < flag
+     end function flag_lt_state
+     elemental logical function flag_le_state(flag,err)
+        integer, intent(in) :: flag
+        type(stdlib_state), intent(in) :: err
+        flag_le_state = err%ierr <= flag
+     end function flag_le_state
+     elemental logical function state_gt_flag(err,flag)
+        type(stdlib_state), intent(in) :: err
+        integer, intent(in) :: flag
+        state_gt_flag = err%ierr > flag
+     end function state_gt_flag
+     elemental logical function state_ge_flag(err,flag)
+        type(stdlib_state), intent(in) :: err
+        integer, intent(in) :: flag
+        state_ge_flag = err%ierr >= flag
+     end function state_ge_flag
+     elemental logical function flag_gt_state(flag,err)
+        integer, intent(in) :: flag
+        type(stdlib_state), intent(in) :: err
+        flag_gt_state = err%ierr > flag
+     end function flag_gt_state
+     elemental logical function flag_ge_state(flag,err)
+        integer, intent(in) :: flag
+        type(stdlib_state), intent(in) :: err
+        flag_ge_state = err%ierr >= flag
+     end function flag_ge_state
 
 end module stdlib_linalg_state
