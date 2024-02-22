e6e8a6a46861516b2e6d7a3531cd366fa2e6b01e
Federico Perini <federico.perini@gmail.com>
2024-02-22 10:12:24 +0100

    `complex` tests

--- test/test_linalg_determinant.f90
+++ test/test_linalg_determinant.f90
@@ -1,3 +1,4 @@
+
 ! Test matrix determinant
 module test_linalg_determinant
     use stdlib_linalg_interface
@@ -16,22 +17,41 @@ module test_linalg_determinant
 
         call test_s_eye_determinant(error)
         if (error) return
+
         call test_s_eye_multiple(error)
         if (error) return
         call test_d_eye_determinant(error)
         if (error) return
+
         call test_d_eye_multiple(error)
         if (error) return
         call test_q_eye_determinant(error)
         if (error) return
+
         call test_q_eye_multiple(error)
         if (error) return
-!        call test_c_eye_inverse(error)
-!        if (error) return
-!        call test_z_eye_inverse(error)
-!        if (error) return
-!        call test_w_eye_inverse(error)
-!        if (error) return
+        call test_c_eye_determinant(error)
+        if (error) return
+
+        call test_c_eye_multiple(error)
+        if (error) return
+        call test_z_eye_determinant(error)
+        if (error) return
+
+        call test_z_eye_multiple(error)
+        if (error) return
+        call test_w_eye_determinant(error)
+        if (error) return
+
+        call test_w_eye_multiple(error)
+        if (error) return
+
+        call test_c_complex_determinant(error)
+        if (error) return
+        call test_z_complex_determinant(error)
+        if (error) return
+        call test_w_complex_determinant(error)
+        if (error) return
 
         call cpu_time(t1)
 
@@ -47,13 +67,14 @@ module test_linalg_determinant
 
         type(linalg_state) :: state
 
-        integer(ilp) :: i,j
+        integer(ilp) :: i
         integer(ilp),parameter :: n = 128_ilp
 
         real(sp) :: a(n,n),deta
 
-        do concurrent(i=1:n,j=1:n)
-          a(i,j) = merge(1.0_sp,0.0_sp,i == j)
+        a = 0.0_sp
+        do concurrent(i=1:n)
+          a(i,i) = 1.0_sp
         end do
 
         !> Determinant function
@@ -63,6 +84,7 @@ module test_linalg_determinant
 
     end subroutine test_s_eye_determinant
 
+    !> Determinant of identity matrix multiplier
     subroutine test_s_eye_multiple(error)
         logical,intent(out) :: error
 
@@ -74,15 +96,14 @@ module test_linalg_determinant
         real(sp) :: a(n,n),deta
 
         !> Multiply eye by a very small number
-        do concurrent(i=1:n,j=1:n)
-          a(i,j) = merge(coef,0.0_sp,i == j)
+        a = 0.0_sp
+        do concurrent(i=1:n)
+          a(i,i) = coef
         end do
 
         !> Determinant: small, but a is not singular, because it is a multiple of the identity.
         deta = det(a,err=state)
         error = state%error() .or. .not. abs(deta - coef**n) < max(tiny(0.0_sp),epsilon(0.0_sp)*coef**n)
-        print *, 'state = ',state%print()
-        print *, 'deta = ',deta,'coef=',coef,' coef**n=',coef**n,' abs=',abs(deta - coef**n),' eps=',epsilon(0.0_sp)*coef**n
         if (error) return
 
     end subroutine test_s_eye_multiple
@@ -92,13 +113,14 @@ module test_linalg_determinant
 
         type(linalg_state) :: state
 
-        integer(ilp) :: i,j
+        integer(ilp) :: i
         integer(ilp),parameter :: n = 128_ilp
 
         real(dp) :: a(n,n),deta
 
-        do concurrent(i=1:n,j=1:n)
-          a(i,j) = merge(1.0_dp,0.0_dp,i == j)
+        a = 0.0_dp
+        do concurrent(i=1:n)
+          a(i,i) = 1.0_dp
         end do
 
         !> Determinant function
@@ -108,6 +130,7 @@ module test_linalg_determinant
 
     end subroutine test_d_eye_determinant
 
+    !> Determinant of identity matrix multiplier
     subroutine test_d_eye_multiple(error)
         logical,intent(out) :: error
 
@@ -119,15 +142,14 @@ module test_linalg_determinant
         real(dp) :: a(n,n),deta
 
         !> Multiply eye by a very small number
-        do concurrent(i=1:n,j=1:n)
-          a(i,j) = merge(coef,0.0_dp,i == j)
+        a = 0.0_dp
+        do concurrent(i=1:n)
+          a(i,i) = coef
         end do
 
         !> Determinant: small, but a is not singular, because it is a multiple of the identity.
         deta = det(a,err=state)
         error = state%error() .or. .not. abs(deta - coef**n) < max(tiny(0.0_dp),epsilon(0.0_dp)*coef**n)
-        print *, 'state = ',state%print()
-        print *, 'deta = ',deta,'coef=',coef,' coef**n=',coef**n,' abs=',abs(deta - coef**n),' eps=',epsilon(0.0_dp)*coef**n
         if (error) return
 
     end subroutine test_d_eye_multiple
@@ -137,13 +159,14 @@ module test_linalg_determinant
 
         type(linalg_state) :: state
 
-        integer(ilp) :: i,j
+        integer(ilp) :: i
         integer(ilp),parameter :: n = 128_ilp
 
         real(qp) :: a(n,n),deta
 
-        do concurrent(i=1:n,j=1:n)
-          a(i,j) = merge(1.0_qp,0.0_qp,i == j)
+        a = 0.0_qp
+        do concurrent(i=1:n)
+          a(i,i) = 1.0_qp
         end do
 
         !> Determinant function
@@ -153,6 +176,7 @@ module test_linalg_determinant
 
     end subroutine test_q_eye_determinant
 
+    !> Determinant of identity matrix multiplier
     subroutine test_q_eye_multiple(error)
         logical,intent(out) :: error
 
@@ -164,127 +188,261 @@ module test_linalg_determinant
         real(qp) :: a(n,n),deta
 
         !> Multiply eye by a very small number
-        do concurrent(i=1:n,j=1:n)
-          a(i,j) = merge(coef,0.0_qp,i == j)
+        a = 0.0_qp
+        do concurrent(i=1:n)
+          a(i,i) = coef
         end do
 
         !> Determinant: small, but a is not singular, because it is a multiple of the identity.
         deta = det(a,err=state)
         error = state%error() .or. .not. abs(deta - coef**n) < max(tiny(0.0_qp),epsilon(0.0_qp)*coef**n)
-        print *, 'state = ',state%print()
-        print *, 'deta = ',deta,'coef=',coef,' coef**n=',coef**n,' abs=',abs(deta - coef**n),' eps=',epsilon(0.0_qp)*coef**n
         if (error) return
 
     end subroutine test_q_eye_multiple
 
-    !> Determinant of identity matrix
     subroutine test_c_eye_determinant(error)
         logical,intent(out) :: error
 
         type(linalg_state) :: state
 
-        integer(ilp) :: i,j,failed
-        integer(ilp),parameter :: n = 250_ilp
+        integer(ilp) :: i
+        integer(ilp),parameter :: n = 128_ilp
 
-        complex(sp) :: a(n,n),copya(n,n),inva(n,n)
+        complex(sp) :: a(n,n),deta
 
-        ! Get eye matrix
-        do concurrent(i=1:n,j=1:n)
-          a(i,j) = merge((1.0_sp,1.0_sp), (0.0_sp,0.0_sp),i == j)
+        a = 0.0_sp
+        do concurrent(i=1:n)
+          a(i,i) = 1.0_sp
         end do
-        copya = a
 
-        !> The inverse of a complex diagonal matrix has conjg(z_ii)/abs(z_ii)^2 on the diagonal
-        inva = det(a,err=state)
-
-        error = state%error() .or. .not. failed == 0
+        !> Determinant function
+        deta = det(a,err=state)
+        error = state%error() .or. .not. abs(deta - 1.0_sp) < tiny(0.0_sp)
         if (error) return
 
-        !> Inverse subroutine
-        call invert(copya,err=state)
-!
-!        failed = 0
-!        do i=1,n
-!            do j=1,n
-!                if (.not.is_diagonal_inverse(a(i,j),copya(i,j),i,j)) failed = failed+1
-!            end do
-!        end do
-
-        error = state%error() .or. .not. failed == 0
-
     end subroutine test_c_eye_determinant
+
+    !> Determinant of identity matrix multiplier
+    subroutine test_c_eye_multiple(error)
+        logical,intent(out) :: error
+
+        type(linalg_state) :: state
+
+        integer(ilp),parameter :: n = 10_ilp
+        real(sp),parameter :: coef = 0.0001_sp
+        integer(ilp) :: i,j
+        complex(sp) :: a(n,n),deta
+
+        !> Multiply eye by a very small number
+        a = 0.0_sp
+        do concurrent(i=1:n)
+          a(i,i) = coef
+        end do
+
+        !> Determinant: small, but a is not singular, because it is a multiple of the identity.
+        deta = det(a,err=state)
+        error = state%error() .or. .not. abs(deta - coef**n) < max(tiny(0.0_sp),epsilon(0.0_sp)*coef**n)
+        if (error) return
+
+    end subroutine test_c_eye_multiple
 
     subroutine test_z_eye_determinant(error)
         logical,intent(out) :: error
 
         type(linalg_state) :: state
 
-        integer(ilp) :: i,j,failed
-        integer(ilp),parameter :: n = 250_ilp
+        integer(ilp) :: i
+        integer(ilp),parameter :: n = 128_ilp
 
-        complex(dp) :: a(n,n),copya(n,n),inva(n,n)
+        complex(dp) :: a(n,n),deta
 
-        ! Get eye matrix
-        do concurrent(i=1:n,j=1:n)
-          a(i,j) = merge((1.0_dp,1.0_dp), (0.0_dp,0.0_dp),i == j)
+        a = 0.0_dp
+        do concurrent(i=1:n)
+          a(i,i) = 1.0_dp
         end do
-        copya = a
 
-        !> The inverse of a complex diagonal matrix has conjg(z_ii)/abs(z_ii)^2 on the diagonal
-        inva = det(a,err=state)
-
-        error = state%error() .or. .not. failed == 0
+        !> Determinant function
+        deta = det(a,err=state)
+        error = state%error() .or. .not. abs(deta - 1.0_dp) < tiny(0.0_dp)
         if (error) return
 
-        !> Inverse subroutine
-        call invert(copya,err=state)
-!
-!        failed = 0
-!        do i=1,n
-!            do j=1,n
-!                if (.not.is_diagonal_inverse(a(i,j),copya(i,j),i,j)) failed = failed+1
-!            end do
-!        end do
-
-        error = state%error() .or. .not. failed == 0
-
     end subroutine test_z_eye_determinant
+
+    !> Determinant of identity matrix multiplier
+    subroutine test_z_eye_multiple(error)
+        logical,intent(out) :: error
+
+        type(linalg_state) :: state
+
+        integer(ilp),parameter :: n = 10_ilp
+        real(dp),parameter :: coef = 0.0001_dp
+        integer(ilp) :: i,j
+        complex(dp) :: a(n,n),deta
+
+        !> Multiply eye by a very small number
+        a = 0.0_dp
+        do concurrent(i=1:n)
+          a(i,i) = coef
+        end do
+
+        !> Determinant: small, but a is not singular, because it is a multiple of the identity.
+        deta = det(a,err=state)
+        error = state%error() .or. .not. abs(deta - coef**n) < max(tiny(0.0_dp),epsilon(0.0_dp)*coef**n)
+        if (error) return
+
+    end subroutine test_z_eye_multiple
 
     subroutine test_w_eye_determinant(error)
         logical,intent(out) :: error
 
         type(linalg_state) :: state
 
-        integer(ilp) :: i,j,failed
-        integer(ilp),parameter :: n = 250_ilp
+        integer(ilp) :: i
+        integer(ilp),parameter :: n = 128_ilp
 
-        complex(qp) :: a(n,n),copya(n,n),inva(n,n)
+        complex(qp) :: a(n,n),deta
 
-        ! Get eye matrix
-        do concurrent(i=1:n,j=1:n)
-          a(i,j) = merge((1.0_qp,1.0_qp), (0.0_qp,0.0_qp),i == j)
+        a = 0.0_qp
+        do concurrent(i=1:n)
+          a(i,i) = 1.0_qp
         end do
-        copya = a
 
-        !> The inverse of a complex diagonal matrix has conjg(z_ii)/abs(z_ii)^2 on the diagonal
-        inva = det(a,err=state)
-
-        error = state%error() .or. .not. failed == 0
+        !> Determinant function
+        deta = det(a,err=state)
+        error = state%error() .or. .not. abs(deta - 1.0_qp) < tiny(0.0_qp)
         if (error) return
 
-        !> Inverse subroutine
-        call invert(copya,err=state)
-!
-!        failed = 0
-!        do i=1,n
-!            do j=1,n
-!                if (.not.is_diagonal_inverse(a(i,j),copya(i,j),i,j)) failed = failed+1
-!            end do
-!        end do
-
-        error = state%error() .or. .not. failed == 0
-
     end subroutine test_w_eye_determinant
+
+    !> Determinant of identity matrix multiplier
+    subroutine test_w_eye_multiple(error)
+        logical,intent(out) :: error
+
+        type(linalg_state) :: state
+
+        integer(ilp),parameter :: n = 10_ilp
+        real(qp),parameter :: coef = 0.0001_qp
+        integer(ilp) :: i,j
+        complex(qp) :: a(n,n),deta
+
+        !> Multiply eye by a very small number
+        a = 0.0_qp
+        do concurrent(i=1:n)
+          a(i,i) = coef
+        end do
+
+        !> Determinant: small, but a is not singular, because it is a multiple of the identity.
+        deta = det(a,err=state)
+        error = state%error() .or. .not. abs(deta - coef**n) < max(tiny(0.0_qp),epsilon(0.0_qp)*coef**n)
+        if (error) return
+
+    end subroutine test_w_eye_multiple
+
+    !> Determinant of complex identity matrix
+    subroutine test_c_complex_determinant(error)
+        logical,intent(out) :: error
+
+        type(linalg_state) :: state
+
+        integer(ilp) :: i,j,n
+        integer(ilp),parameter :: nmax = 10_ilp
+
+        complex(sp),parameter :: res(nmax) = [complex(sp) ::(1,1), (0,2), (-2,2), (-4,0), (-4,-4), &
+                                                  (0,-8), (8,-8), (16,0), (16,16), (0,32)]
+
+        complex(sp),allocatable :: a(:,:)
+        complex(sp) :: deta(nmax)
+
+        !> Test determinant for all sizes, 1:nmax
+        matrix_size: do n = 1,nmax
+
+           ! Put 1+i on each diagonal element
+           allocate (a(n,n))
+           do concurrent(i=1:n,j=1:n)
+             a(i,j) = merge((1.0_sp,1.0_sp), (0.0_sp,0.0_sp),i == j)
+           end do
+
+           ! Expected result
+           deta(n) = det(a,err=state)
+
+           deallocate (a)
+           if (state%error()) exit matrix_size
+
+        end do matrix_size
+
+        error = state%error() .or. any(.not. abs(res - deta) <= tiny(0.0_sp))
+
+    end subroutine test_c_complex_determinant
+
+    subroutine test_z_complex_determinant(error)
+        logical,intent(out) :: error
+
+        type(linalg_state) :: state
+
+        integer(ilp) :: i,j,n
+        integer(ilp),parameter :: nmax = 10_ilp
+
+        complex(dp),parameter :: res(nmax) = [complex(dp) ::(1,1), (0,2), (-2,2), (-4,0), (-4,-4), &
+                                                  (0,-8), (8,-8), (16,0), (16,16), (0,32)]
+
+        complex(dp),allocatable :: a(:,:)
+        complex(dp) :: deta(nmax)
+
+        !> Test determinant for all sizes, 1:nmax
+        matrix_size: do n = 1,nmax
+
+           ! Put 1+i on each diagonal element
+           allocate (a(n,n))
+           do concurrent(i=1:n,j=1:n)
+             a(i,j) = merge((1.0_dp,1.0_dp), (0.0_dp,0.0_dp),i == j)
+           end do
+
+           ! Expected result
+           deta(n) = det(a,err=state)
+
+           deallocate (a)
+           if (state%error()) exit matrix_size
+
+        end do matrix_size
+
+        error = state%error() .or. any(.not. abs(res - deta) <= tiny(0.0_dp))
+
+    end subroutine test_z_complex_determinant
+
+    subroutine test_w_complex_determinant(error)
+        logical,intent(out) :: error
+
+        type(linalg_state) :: state
+
+        integer(ilp) :: i,j,n
+        integer(ilp),parameter :: nmax = 10_ilp
+
+        complex(qp),parameter :: res(nmax) = [complex(qp) ::(1,1), (0,2), (-2,2), (-4,0), (-4,-4), &
+                                                  (0,-8), (8,-8), (16,0), (16,16), (0,32)]
+
+        complex(qp),allocatable :: a(:,:)
+        complex(qp) :: deta(nmax)
+
+        !> Test determinant for all sizes, 1:nmax
+        matrix_size: do n = 1,nmax
+
+           ! Put 1+i on each diagonal element
+           allocate (a(n,n))
+           do concurrent(i=1:n,j=1:n)
+             a(i,j) = merge((1.0_qp,1.0_qp), (0.0_qp,0.0_qp),i == j)
+           end do
+
+           ! Expected result
+           deta(n) = det(a,err=state)
+
+           deallocate (a)
+           if (state%error()) exit matrix_size
+
+        end do matrix_size
+
+        error = state%error() .or. any(.not. abs(res - deta) <= tiny(0.0_qp))
+
+    end subroutine test_w_complex_determinant
 
 end module test_linalg_determinant
 
