c211b2d2d4fee344d9b08bfe105815474cd9c999
perazz <perazz@users.noreply.github.com>
2025-01-31 15:25:59 +0000

    deploy: 4d3858260ceaaba5baba499ef2f390af7ae567d8

--- search/all_0.js
+++ search/all_0.js
@@ -1,14 +1,15 @@
 var searchData=
 [
-  ['a_20lambda_20tt_0',['A lambda tt',['../index.html#autotoc_md29',1,'&lt;tt&gt;eig(A, lambda)&lt;/tt&gt;'],['../index.html#autotoc_md31',1,'&lt;tt&gt;eigh(A, lambda)&lt;/tt&gt;']]],
+  ['a_20lambda_20tt_0',['A lambda tt',['../index.html#autotoc_md48',1,'&lt;tt&gt;eig(A, lambda)&lt;/tt&gt;'],['../index.html#autotoc_md50',1,'&lt;tt&gt;eigh(A, lambda)&lt;/tt&gt;']]],
   ['a_20least_20squares_20solution_20to_20a_20system_20of_20linear_20equations_1',['@ref la_least_squares::lstsq &quot;lstsq&quot; - Compute a least squares solution to a system of linear equations.',['../index.html#autotoc_md12',1,'']]],
   ['a_20linear_20matrix_20equation_20or_20a_20linear_20system_20of_20equations_2',['@ref la_solve::solve &quot;solve&quot; - Solve a linear matrix equation or a linear system of equations.',['../index.html#autotoc_md6',1,'']]],
   ['a_20linear_20system_20of_20equations_3',['@ref la_solve::solve &quot;solve&quot; - Solve a linear matrix equation or a linear system of equations.',['../index.html#autotoc_md6',1,'']]],
-  ['a_20matrix_4',['@ref la_qr::qr &quot;qr&quot; - Compute the QR factorization of a matrix.',['../index.html#autotoc_md34',1,'']]],
-  ['a_20system_20of_20linear_20equations_5',['@ref la_least_squares::lstsq &quot;lstsq&quot; - Compute a least squares solution to a system of linear equations.',['../index.html#autotoc_md12',1,'']]],
-  ['a_20tt_6',['A tt',['../index.html#autotoc_md23',1,'&lt;tt&gt;.inv.A&lt;/tt&gt;'],['../index.html#autotoc_md24',1,'&lt;tt&gt;.pinv.A&lt;/tt&gt;'],['../index.html#autotoc_md19',1,'&lt;tt&gt;det(A)&lt;/tt&gt;'],['../index.html#autotoc_md28',1,'&lt;tt&gt;eigvals(A)&lt;/tt&gt;'],['../index.html#autotoc_md30',1,'&lt;tt&gt;eigvalsh(A)&lt;/tt&gt;'],['../index.html#autotoc_md20',1,'&lt;tt&gt;inv(A)&lt;/tt&gt;'],['../index.html#autotoc_md22',1,'&lt;tt&gt;invert(A)&lt;/tt&gt;'],['../index.html#autotoc_md21',1,'&lt;tt&gt;pinv(A)&lt;/tt&gt;'],['../index.html#autotoc_md25',1,'&lt;tt&gt;svd(A)&lt;/tt&gt;'],['../index.html#autotoc_md26',1,'&lt;tt&gt;svdvals(A)&lt;/tt&gt;']]],
-  ['acknowledgments_7',['Acknowledgments',['../index.html#autotoc_md52',1,'']]],
-  ['api_8',['Browse API',['../index.html#autotoc_md5',1,'']]],
-  ['arguments_9',['Arguments',['../index.html#autotoc_md9',1,'Arguments'],['../index.html#autotoc_md15',1,'Arguments'],['../index.html#autotoc_md37',1,'Arguments'],['../index.html#autotoc_md44',1,'Arguments']]],
-  ['axpy_10',['axpy',['../interfacela__blas_1_1axpy.html',1,'la_blas']]]
+  ['a_20matrix_4',['a matrix',['../index.html#autotoc_md37',1,'@ref la_pseudoinverse::operator(.pinv.) &quot;operator(.pinv.)&quot; - Compute the Moore-Penrose pseudo-inverse of a matrix.'],['../index.html#autotoc_md23',1,'@ref la_pseudoinverse::pseudoinvert &quot;pseudoinvert&quot; - Moore-Penrose pseudo-inverse of a matrix.'],['../index.html#autotoc_md53',1,'@ref la_qr::qr &quot;qr&quot; - Compute the QR factorization of a matrix.']]],
+  ['a_20matrix_20function_5',['@ref la_pseudoinverse::pinv &quot;pinv&quot; - Moore-Penrose pseudo-inverse of a matrix (function).',['../index.html#autotoc_md30',1,'']]],
+  ['a_20system_20of_20linear_20equations_6',['@ref la_least_squares::lstsq &quot;lstsq&quot; - Compute a least squares solution to a system of linear equations.',['../index.html#autotoc_md12',1,'']]],
+  ['a_20tt_7',['A tt',['../index.html#autotoc_md22',1,'&lt;tt&gt;.inv.A&lt;/tt&gt;'],['../index.html#autotoc_md19',1,'&lt;tt&gt;det(A)&lt;/tt&gt;'],['../index.html#autotoc_md47',1,'&lt;tt&gt;eigvals(A)&lt;/tt&gt;'],['../index.html#autotoc_md49',1,'&lt;tt&gt;eigvalsh(A)&lt;/tt&gt;'],['../index.html#autotoc_md20',1,'&lt;tt&gt;inv(A)&lt;/tt&gt;'],['../index.html#autotoc_md21',1,'&lt;tt&gt;invert(A)&lt;/tt&gt;'],['../index.html#autotoc_md44',1,'&lt;tt&gt;svd(A)&lt;/tt&gt;'],['../index.html#autotoc_md45',1,'&lt;tt&gt;svdvals(A)&lt;/tt&gt;']]],
+  ['acknowledgments_8',['Acknowledgments',['../index.html#autotoc_md71',1,'']]],
+  ['api_9',['Browse API',['../index.html#autotoc_md5',1,'']]],
+  ['arguments_10',['Arguments',['../index.html#autotoc_md9',1,'Arguments'],['../index.html#autotoc_md15',1,'Arguments'],['../index.html#autotoc_md26',1,'Arguments'],['../index.html#autotoc_md33',1,'Arguments'],['../index.html#autotoc_md40',1,'Arguments'],['../index.html#autotoc_md56',1,'Arguments'],['../index.html#autotoc_md63',1,'Arguments']]],
+  ['axpy_11',['axpy',['../interfacela__blas_1_1axpy.html',1,'la_blas']]]
 ];
--- search/all_f.js
+++ search/all_f.js
@@ -2,13 +2,16 @@ var searchData=
 [
   ['readme_2emd_0',['README.md',['../_r_e_a_d_m_e_8md.html',1,'']]],
   ['ref_20la_5fleast_5fsquares_3a_3alstsq_20lstsq_20compute_20a_20least_20squares_20solution_20to_20a_20system_20of_20linear_20equations_1',['@ref la_least_squares::lstsq &quot;lstsq&quot; - Compute a least squares solution to a system of linear equations.',['../index.html#autotoc_md12',1,'']]],
-  ['ref_20la_5fqr_3a_3aqr_20qr_20compute_20the_20qr_20factorization_20of_20a_20matrix_2',['@ref la_qr::qr &quot;qr&quot; - Compute the QR factorization of a matrix.',['../index.html#autotoc_md34',1,'']]],
-  ['ref_20la_5fqr_3a_3aqr_5fspace_20qr_5fspace_20workspace_20size_20for_20qr_20operations_3',['@ref la_qr::qr_space &quot;qr_space&quot; - Workspace size for QR operations.',['../index.html#autotoc_md41',1,'']]],
-  ['ref_20la_5fsolve_3a_3asolve_20solve_20solve_20a_20linear_20matrix_20equation_20or_20a_20linear_20system_20of_20equations_4',['@ref la_solve::solve &quot;solve&quot; - Solve a linear matrix equation or a linear system of equations.',['../index.html#autotoc_md6',1,'']]],
-  ['return_20value_5',['Return value',['../index.html#autotoc_md10',1,'Return value'],['../index.html#autotoc_md16',1,'Return value'],['../index.html#autotoc_md38',1,'Return value'],['../index.html#autotoc_md45',1,'Return value']]],
-  ['rot_6',['rot',['../interfacela__blas_1_1rot.html',1,'la_blas::rot'],['../interfacela__lapack_1_1rot.html',1,'la_lapack::rot']]],
-  ['rotg_7',['rotg',['../interfacela__blas_1_1rotg.html',1,'la_blas']]],
-  ['rotm_8',['rotm',['../interfacela__blas_1_1rotm.html',1,'la_blas']]],
-  ['rotmg_9',['rotmg',['../interfacela__blas_1_1rotmg.html',1,'la_blas']]],
-  ['rscl_10',['rscl',['../interfacela__lapack_1_1rscl.html',1,'la_lapack']]]
+  ['ref_20la_5fpseudoinverse_3a_3aoperator_20pinv_20operator_20pinv_20compute_20the_20moore_20penrose_20pseudo_20inverse_20of_20a_20matrix_2',['@ref la_pseudoinverse::operator(.pinv.) &quot;operator(.pinv.)&quot; - Compute the Moore-Penrose pseudo-inverse of a matrix.',['../index.html#autotoc_md37',1,'']]],
+  ['ref_20la_5fpseudoinverse_3a_3apinv_20pinv_20moore_20penrose_20pseudo_20inverse_20of_20a_20matrix_20function_3',['@ref la_pseudoinverse::pinv &quot;pinv&quot; - Moore-Penrose pseudo-inverse of a matrix (function).',['../index.html#autotoc_md30',1,'']]],
+  ['ref_20la_5fpseudoinverse_3a_3apseudoinvert_20pseudoinvert_20moore_20penrose_20pseudo_20inverse_20of_20a_20matrix_4',['@ref la_pseudoinverse::pseudoinvert &quot;pseudoinvert&quot; - Moore-Penrose pseudo-inverse of a matrix.',['../index.html#autotoc_md23',1,'']]],
+  ['ref_20la_5fqr_3a_3aqr_20qr_20compute_20the_20qr_20factorization_20of_20a_20matrix_5',['@ref la_qr::qr &quot;qr&quot; - Compute the QR factorization of a matrix.',['../index.html#autotoc_md53',1,'']]],
+  ['ref_20la_5fqr_3a_3aqr_5fspace_20qr_5fspace_20workspace_20size_20for_20qr_20operations_6',['@ref la_qr::qr_space &quot;qr_space&quot; - Workspace size for QR operations.',['../index.html#autotoc_md60',1,'']]],
+  ['ref_20la_5fsolve_3a_3asolve_20solve_20solve_20a_20linear_20matrix_20equation_20or_20a_20linear_20system_20of_20equations_7',['@ref la_solve::solve &quot;solve&quot; - Solve a linear matrix equation or a linear system of equations.',['../index.html#autotoc_md6',1,'']]],
+  ['return_20value_8',['Return value',['../index.html#autotoc_md10',1,'Return value'],['../index.html#autotoc_md16',1,'Return value'],['../index.html#autotoc_md27',1,'Return value'],['../index.html#autotoc_md34',1,'Return value'],['../index.html#autotoc_md41',1,'Return value'],['../index.html#autotoc_md57',1,'Return value'],['../index.html#autotoc_md64',1,'Return value']]],
+  ['rot_9',['rot',['../interfacela__blas_1_1rot.html',1,'la_blas::rot'],['../interfacela__lapack_1_1rot.html',1,'la_lapack::rot']]],
+  ['rotg_10',['rotg',['../interfacela__blas_1_1rotg.html',1,'la_blas']]],
+  ['rotm_11',['rotm',['../interfacela__blas_1_1rotm.html',1,'la_blas']]],
+  ['rotmg_12',['rotmg',['../interfacela__blas_1_1rotmg.html',1,'la_blas']]],
+  ['rscl_13',['rscl',['../interfacela__lapack_1_1rscl.html',1,'la_lapack']]]
 ];
